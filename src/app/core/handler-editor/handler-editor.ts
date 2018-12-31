fb1d286c96b005958330186c1189c9fddc74d4cc
Alejandro de Tovar <venespana159@gmail.com>
2018-12-31 15:14:42 +0100

    Added generic save point

---
+++ src/app/core/handler-editor/handler-editor.ts
@@ -0,0 +1,13 @@
+export class HandlerEditor {
+
+    public static saveDoc(contentTag, content, args) {
+        args.service.save(
+            contentTag,
+            content,
+            'Change section ' +
+            args.node
+                .getSection()
+                .getAttribute('xe_section')
+        );
+    }
+}
