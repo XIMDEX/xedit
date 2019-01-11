ff92000e859ee979a212ded88f17712c2c7a1efa
Alejandro de Tovar <venespana159@gmail.com>
2019-01-11 15:31:09 +0100

    Minor updates in components

--- src/app/elements/xedit/ckeditor/ckeditor.component.ts
+++ src/app/elements/xedit/ckeditor/ckeditor.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, Input, Output, EventEmitter, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
-import * as InlineEditor from '@ckeditor/ckeditor5-build-inline';
 import * as DecoupledEditor from '@ckeditor/ckeditor5-build-decoupled-document';
 import { BlurEvent } from '@ckeditor/ckeditor5-angular/ckeditor.component';
 import { XeditBaseComponent } from '../xedit.base.component';
+import { isNil, hasIn, equals, union, uniq } from 'ramda';
+
 
 @Component({
     selector: 'app-ckeditor',
@@ -13,13 +14,19 @@ import { XeditBaseComponent } from '../xedit.base.component';
 export class CkeditorComponent extends XeditBaseComponent implements OnInit, OnDestroy {
 
     public editor;
+    private toolbarMenu;
+    private config = {};
+    private availabelPlugins = []
 
     constructor() { 
         super();
     }
 
     ngOnInit() {
-        this.editor = InlineEditor;
+        this.editor = DecoupledEditor;
+        this.toolbarMenu = document.querySelector('#toolbar');
+        this.config['extraPlugins'] = [];
+        this.config['toolbar'] = this.generateToolbar();
     }
 
     ngOnDestroy() {
@@ -28,7 +35,9 @@ export class CkeditorComponent extends XeditBaseComponent implements OnInit, OnD
 
     onBlur({ editor: evt }: BlurEvent) {
         const { uuid } = this.content;
-        const { element } = evt;
+        const { element: srcElement, sourceElement } = evt;
+
+        const element = isNil(srcElement) ? sourceElement : srcElement;
 
         this.onChange.emit({
             uuid,
@@ -36,15 +45,161 @@ export class CkeditorComponent extends XeditBaseComponent implements OnInit, OnD
             content: evt.getData()
         });
         this.selectNode.emit(null);
+        this.toolbarMenu.removeChild(evt.ui.view.toolbar.element);
     }
 
-    onFocus() {
+    onFocus({ editor }) {
         const { uuid } = this.content;
         this.selectNode.emit(uuid);
+        this.toolbarMenu.appendChild(editor.ui.view.toolbar.element);
     }
 
     onClick(evt: MouseEvent) {
         evt.stopPropagation();
+        this.toolbar.emit(null);
+    }
+
+    private generateToolbar() {
+        let toolbar = [];
+        const options = this.content.settings.options;
+        const { styles } = options;
+
+        if (!isNil(styles)) {
+            toolbar = toolbar.concat(this.toolbarStyles(styles));
+        }
+        return toolbar;
+    }
+
+    private toolbarStyles(styles: Array<string> | string) {
+        const stylesValue = {};
+        const groups = {
+            group1: {
+                bold: 'bold',
+                italic: 'italic',
+                underline: 'underline',
+                strikethrough: 'strikethrough',
+                // color: 'color',
+                // background: 'background',
+            },
+            others: {
+                ol: 'numberedList',
+                ul: 'bulletedList',
+                table: 'insertTable',
+            },
+            align: {
+                alignleft: 'alignment:left',
+                aligncenter: 'alignment:center',
+                alignright: 'alignment:right',
+                alignjustify: 'alignment:justify',
+            },
+            // indent: {
+            //     outdent: 'outdent',
+            //     indent: 'indent',
+            // },
+            // format: {
+            //     formatselect: 'formatselect',
+            // },
+            font: {
+                fontselect: 'fontFamily',
+                fontsize: 'fontSize',
+            },
+        };
+
+        if (typeof styles === 'string') {
+            styles = equals(styles, 'all')
+                ? Object.keys(groups)
+                : [];
+        }
+
+        styles.forEach(style => {
+            if (hasIn(style, groups)) {
+                this.addValue(
+                    stylesValue,
+                    style,
+                    Object.values(groups[style])
+                );
+            } else {
+                for (const group in groups) {
+                    if (hasIn(style, groups[group])) {
+                        this.addValue(stylesValue, group, [
+                            groups[group][style],
+                        ]);
+                    }
+                }
+            }
+        });
+
+        let result = [];
+        for (const styleValue in stylesValue) {
+            if (!isNil(stylesValue[styleValue])) {
+                result = result.concat(uniq(stylesValue[styleValue]), ['|']);
+            }
+        }
+
+        for (const value of result) {
+            this.addPluginSettings(value);
+        }
+
+        return result;
+    }
+
+    private addValue(
+        object: Object,
+        property: string,
+        value: Array<string> | string
+    ) {
+        if (hasIn(property, object)) {
+            object[property] = union(object[property], value);
+        } else {
+            object[property] = value;
+        }
+    }
+
+    private addPlugin(plugin: string) {
+        for (const plug of this.availabelPlugins) {
+            const name = (typeof plug === 'string') ? plug.toLowerCase() : plug.pluginName.toLowerCase();
+            if (name === plugin.toLowerCase()) {
+                this.config['extraPlugins'].push(plug);
+                break;
+            }
+        }
+    }
+
+    private addPluginSettings(plugin: string) {
+        this.addPlugin(plugin);
+        if (hasIn(plugin, this) && typeof this[plugin] === 'function') {
+            this.config[plugin] = this[plugin]();
+        }
+    }
+
+    private fontSize() {
+        return {
+            options: [
+                '9pt',
+                '11pt',
+                '13pt',
+                '14pt',
+                '17pt',
+                '19pt',
+                '21pt'
+            ]
+        }
+    }
+
+    private fontFamily() {
+        const fonts = [
+            'default',
+            'Arial, Helvetica, sans-serif',
+            'Courier New, Courier, monospace',
+            'Georgia, serif',
+            'Lucida Sans Unicode, Lucida Grande, sans-serif',
+            'Tahoma, Geneva, sans-serif',
+            'Times New Roman, Times, serif',
+            'Trebuchet MS, Helvetica, sans-serif',
+            'Verdana, Geneva, sans-serif'
+        ];
+
+        return fonts;
     }
 
 }
--- src/app/elements/xedit/image/image-modal/image-modal.component.ts
+++ src/app/elements/xedit/image/image-modal/image-modal.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { NgxSmartModalService } from 'ngx-smart-modal';
-import { ViewEncapsulation } from '@angular/core';
-import { faImage } from '@fortawesome/free-solid-svg-icons';
+import { hasIn, isNil } from 'ramda';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
     selector: 'app-image-modal',
@@ -11,8 +10,97 @@ import { faImage } from '@fortawesome/free-solid-svg-icons';
 })
 export class ImageModalComponent implements OnInit {
 
-    constructor(private ngxModal: NgxSmartModalService) { }
+    public searchIcon = faSearch;
+
+    public file: string = '';
+    public src: string = null;
+    public name: string = null;
+    public size: Dimesion = null;
+
+    private imageStyle: any;
+
+    constructor() { }
 
     ngOnInit() {
     }
+
+    imagePath() {
+        let path: string = null;
+        if (!isNil(this.file)) {
+            path = this.file;
+        }
+
+        return path;
+    }
+
+    hasImagePath() {
+        return !isNil(this.imagePath());
+    }
+
+    fileName() {
+        let name = 'Elemento no seleccionado...';
+        if (!isNil(this.name)) {
+            name = this.name
+        } else if (isNil(this.name) && !isNil(this.file)) {
+            name = this.file;
+        }
+
+        return name;
+    }
+
+    fileRoute() {
+        let route = 'Elemento no seleccionado...';
+        if (!isNil(this.src)) {
+            route = this.src;
+        } else if (isNil(this.src) && !isNil(this.file)) {
+            route = this.file;
+        }
+
+        return route;
+    }
+
+    setFormData(data) {
+        const { fields } = data;
+
+        for (const field of Object.keys(fields)) {
+            if (hasIn(field, this)) {
+                this[field] = fields[field];
+            }
+        }
+    }
+
+    cleanFormData() {
+    }
+
+    setImage(modal) {
+        if (modal.hasData()) {
+            const { save } = modal.getData();
+
+            if (typeof save === 'function') {
+                const data = {
+                    src: this.file,
+                    xe_link: this.file,
+                    style: this.imageStyle
+                };
+                save(data);
+            }
+        }
+
+        this.closeModal(modal);
+    }
+
+    imageData({ left, top, width, height }: Cropper.CanvasData) {
+        this.imageStyle = `position: absolute; left: ${left}px; top: ${top}px; width: ${width}px; height: ${height}px`;
+    }
+
+    closeModal(modal) {
+        modal.close();
+    }
+}
+
+interface Dimesion {
+    width: Number,
+    height: Number;
+    naturalWidth: Number;
+    naturalHeight: Number;
 }
