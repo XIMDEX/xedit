8f29ce55380e6a969b22b46d3329ea72fd2b034f
Alejandro de Tovar <venespana159@gmail.com>
2019-01-17 18:09:53 +0100

    minor fixes in sections

--- src/app/elements/xedit/section/section.component.ts
+++ src/app/elements/xedit/section/section.component.ts
@@ -1,20 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, HostListener, OnChanges, SimpleChanges } from '@angular/core';
 import { XeditBaseComponent } from '../xedit.base.component';
+import { isNil } from 'ramda';
+import { DOM } from '../../../models/dom';
 
 @Component({
     selector: 'app-section',
-    templateUrl: './section.component.html',
+    template: '<ng-content></ng-content>',
     styleUrls: ['./section.component.scss']
 })
-export class SectionComponent extends XeditBaseComponent implements OnInit {
+export class SectionComponent extends XeditBaseComponent implements OnInit, OnChanges {
 
-    constructor() {
+    public static hasSlot: boolean = true;
+
+    constructor(public host: ElementRef) {
         super();
     }
 
     ngOnInit() {
     }
 
+    ngOnChanges({ selected }: SimpleChanges) {
+        if (!isNil(selected) && selected.currentValue !== selected.previousValue) {
+            const element = new DOM(this.host.nativeElement.querySelector('[xe_section]'));
+            if (this.isSelected()) {
+                element.addClass('xe_selected')
+            } else {
+                element.removeClass('xe_selected');
+            }
+        }
+    }
+
+    @HostListener('click', ['$event'])
     onClick(evt: MouseEvent) {
         evt.stopPropagation();
         const { uuid } = this.content;
