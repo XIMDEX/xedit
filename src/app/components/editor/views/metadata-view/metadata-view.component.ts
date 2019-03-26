661e4fe492a63d8cef8aacc589271461484dfede
David Arroyo <arroyoescobardavid@gmail.com>
2019-03-26 12:48:21 +0100

    Support to readonly and required to dyn-questions

--- src/app/components/editor/views/metadata-view/metadata-view.component.ts
+++ src/app/components/editor/views/metadata-view/metadata-view.component.ts
@@ -1,202 +1,106 @@
-import { EditorService } from 'app/services/editor-service/editor.service';
-import { Component, OnInit } from '@angular/core';
-import {  hasIn, isNil } from 'ramda';
-import { File } from '@models/file';
-
+import { EditorService } from "app/services/editor-service/editor.service";
+import { Component, OnInit } from "@angular/core";
+import { hasIn, isNil } from "ramda";
+import { File } from "@models/file";
 
 @Component({
-  selector: 'app-metadata-view',
-  templateUrl: './metadata-view.component.html',
-  styleUrls: ['./metadata-view.component.scss']
+    selector: "app-metadata-view",
+    templateUrl: "./metadata-view.component.html",
+    styleUrls: ["./metadata-view.component.scss"]
 })
 export class MetadataViewComponent implements OnInit {
+    tabs = [];
+    payload: any = {};
+    private file: File;
+    private schema: {};
+    meta: any = {};
 
-  tabs = [];
-  payload: any = {};
-  private file: File;
-  schema =
-    {
-    name: 'lomes',
-    title: 'LOMES',
-    api: false,
-      tabs: [
-        {
-          title: 'Pestaña 1',
-          sections: [
-            {
-              title: 'section 1',
-              fields: [
-                {
-                  object: {
-                    realName: 'one',
-                    key: 'one',
-                    label: 'First Field'
-                  },
-                  type: 'text'
-                },
-                {
-                  object: {
-                    realName: 'onehalf',
-                    key: 'onehalf',
-                    label: 'First and a half Field'
-                  },
-                  type: 'text'
-                },
-                {
-                  object: {
-                    realName: 'two',
-                    key: 'two',
-                    label: 'Second Field',
-                    multi: true,
-                    searchable: true,
-                    options: [
-                      { key: 'option1', value: 'Option 1' },
-                      { key: 'option2', value: 'Option 2' },
-                      { key: 'option3', value: 'Option 3' }
-                    ]
-                  },
-                  type: 'dropdown',
-                }
-              ]
-            }
-          ]
-        }
-          ,
-        {
-          title: 'Pestaña 1',
-          sections: [
-            {
-              title: 'section 2',
-              fields: [
-                {
-                  object: {
-                    realName: 'one',
-                    key: 'one',
-                    label: 'First Field'
-                  },
-                  type: 'text'
-                },
-                {
-                  object: {
-                    realName: 'onehalf',
-                    key: 'onehalf',
-                    label: 'First and a half Field'
-                  },
-                  type: 'text'
-                },
-                {
-                  object: {
-                    realName: 'two',
-                    key: 'two',
-                    label: 'Second Field',
-                    multi: true,
-                    searchable: true,
-                    options: [
-                      { key: 'option1', value: 'Option 1' },
-                      { key: 'option2', value: 'Option 2' },
-                      { key: 'option3', value: 'Option 3' }
-                    ]
-                  },
-                  type: 'dropdown',
-                }
-              ]
-            }
-          ]
-        }
-      ]
-    };
-  meta: any = {};
-
-  constructor(private _edService: EditorService) {
-    this.meta = _edService.getUpdatedDocument();
-  }
-
-  ngOnInit() {
-    this.metaMap(this.meta);
-    this._edService.getFile().subscribe(file => {
-      this.file = file;
-      if (file != null) {
-        file['metadata'] = this.payload;
-      }
-    });
-  }
-
-  formResult(event) {
-    this.payload = this.adaptResponse(event);
-    this.file['metadata'] = this.payload;
-    this._edService.setFile(this.file);
-  }
-
-  adaptResponse(response) {
-    const result = {};
-    Object.keys(response).forEach(function (key) {
-      const ids = key.split('-');
-      const group_id = ids[0];
-      const meta_id = ids[1];
-      if (!hasIn(group_id, result)) {
-        result[group_id] = {};
-      }
-      if (!isNil(response[key])) {
-        result[group_id][meta_id] = response[key];
-      }
-    });
-    return result;
-  }
-
-  mapTab(meta) {
-    const sections = this.mapSections(meta.groups);
-    return(
-      {
-        title: meta.name,
-        sections: sections
-      }
-    );
-  }
-
-  mapTabs(metas) {
-    for (const key of Object.keys(metas)) {
-      this.tabs.push(this.mapTab(metas[key]));
+    constructor(private _edService: EditorService) {
+        this.meta = _edService.getUpdatedDocument();
     }
-  }
 
-  mapSections(groups) {
-    const sections = groups.map((group) => {
-      const fields = this.mapFields(group.metadata, group.id);
-      return (
-        {
-          title: group.name,
-          fields: fields
+    ngOnInit() {
+        this.metaMap(this.meta);
+        this._edService.getFile().subscribe(file => {
+            this.file = file;
+            if (file != null) {
+                file["metadata"] = this.payload;
+            }
+        });
+    }
+
+    formResult(event) {
+        this.payload = this.adaptResponse(event);
+        this.file["metadata"] = this.payload;
+        this._edService.setFile(this.file);
+    }
+
+    adaptResponse(response) {
+        const result = {};
+        Object.keys(response).forEach(function(key) {
+            const ids = key.split("-");
+            const group_id = ids[0];
+            const meta_id = ids[1];
+            if (!hasIn(group_id, result)) {
+                result[group_id] = {};
+            }
+            if (!isNil(response[key])) {
+                result[group_id][meta_id] = response[key];
+            }
+        });
+        return result;
+    }
+
+    mapTab(meta) {
+        const sections = this.mapSections(meta.groups);
+        return {
+            title: meta.name,
+            sections: sections
+        };
+    }
+
+    mapTabs(metas) {
+        for (const key of Object.keys(metas)) {
+            this.tabs.push(this.mapTab(metas[key]));
         }
-      );
-    });
-    return sections;
-  }
+    }
 
-  mapFields(metadata, group_id) {
-    const fields = metadata.map((metafield) => {
-      return (
-        {
-          object: {
-            realName: `${group_id}-${metafield.id}`,
-            key: `${group_id}-${metafield.id}`,
-            label: metafield.name,
-            order: metafield.id,
-            val: metafield.value
-          },
-          type: metafield.type,
-        }
-      );
-    });
-    return fields;
-  }
+    mapSections(groups) {
+        const sections = groups.map(group => {
+            const fields = this.mapFields(group.metadata, group.id);
+            return {
+                title: group.name,
+                fields: fields
+            };
+        });
+        return sections;
+    }
 
-  metaMap(meta) {
-    this.mapTabs(meta.metas);
-    this.schema = {
-      name: 'xedit_meta',
-      title: 'Metadata',
-      api: false,
-      tabs: this.tabs
-    };
-  }
+    mapFields(metadata, group_id) {
+        const fields = metadata.map(metafield => {
+            return {
+                object: {
+                    realName: `${group_id}-${metafield.id}`,
+                    key: `${group_id}-${metafield.id}`,
+                    label: metafield.name,
+                    order: metafield.id,
+                    val: metafield.value,
+                    required: !!metafield.required,
+                    readonly: !!metafield.readonly
+                },
+                type: metafield.type
+            };
+        });
+        return fields;
+    }
+
+    metaMap(meta) {
+        this.mapTabs(meta.metas);
+        this.schema = {
+            name: "xedit_meta",
+            title: "Metadata",
+            api: false,
+            tabs: this.tabs
+        };
+    }
 }
--- src/app/elements/forms/dynform/questions/question-base.ts
+++ src/app/elements/forms/dynform/questions/question-base.ts
@@ -1,68 +1,73 @@
-import { hasIn } from 'ramda';
+import { hasIn } from "ramda";
 /**
  * Represents a simple question with the needed values to work in a form
  */
 export class QuestionBase<T> {
-  /** The parameter for question type */
-  value: T;
-  /** The field key property */
-  key: string;
-  /** The field label property */
-  label: string;
-  /** If the question is required in the form */
-  required: boolean;
-  /** The order of appeareance */
-  order: number;
-  /** The value for switching views */
-  controlType: string;
-  /**@ignore */
-  val: any;
-  /**The value mapping for selectors */
-  map: {key: string, value: string};
+    /** The parameter for question type */
+    value: T;
+    /** The field key property */
+    key: string;
+    /** The field label property */
+    label: string;
+    /** If the question is required in the form */
+    required: boolean;
+    /** If the question is readonly in the form */
+    readonly: boolean;
+    /** The order of appeareance */
+    order: number;
+    /** The value for switching views */
+    controlType: string;
+    /**@ignore */
+    val: any;
+    /**The value mapping for selectors */
+    map: { key: string; value: string };
 
-  /**@ignore */
-  constructor(options: {
-      value?: T,
-      key?: string,
-      label?: string,
-      required?: boolean,
-      order?: number,
-      controlType?: string,
-      val?: any,
-      map?: {key: string, value: string}
-    } = {}) {
-    this.value = options.value;
-    this.key = options.key || '';
-    this.label = options.label || '';
-    this.required = !!options.required;
-    this.order = options.order === undefined ? 1 : options.order;
-    this.controlType = options.controlType || '';
-    const val = options.val || null;
-    this.set(options, 'map', {})
-      .setVal(val);
-  }
-
-  /**
-   * Securely sets the value from the selected object
-   * @param object The object to be used
-   * @param field The field to set the value
-   * @param _default The default value if any
-   */
-  set(object, field: string, _default: any = null): QuestionBase<T> {
-    let value = _default;
-    if (hasIn(field, object)) {
-      value = object[field];
+    /**@ignore */
+    constructor(
+        options: {
+            value?: T;
+            key?: string;
+            label?: string;
+            required?: boolean;
+            readonly?: boolean;
+            order?: number;
+            controlType?: string;
+            val?: any;
+            map?: { key: string; value: string };
+        } = {}
+    ) {
+        this.value = options.value;
+        this.key = options.key || "";
+        this.label = options.label || "";
+        this.required = !!options.required;
+        this.readonly = !!options.readonly;
+        this.order = options.order === undefined ? 1 : options.order;
+        this.controlType = options.controlType || "";
+        const val = options.val || null;
+        this.set(options, "map", {}).setVal(val);
     }
-    this[field] = value;
-    return this;
-  }
 
-  /**
-   * Sets the val property and returns this
-   * @param val The new val value
-   */
-  setVal(val: any = null) {
-    this.val = val;
-    return this;
-  }
+    /**
+     * Securely sets the value from the selected object
+     * @param object The object to be used
+     * @param field The field to set the value
+     * @param _default The default value if any
+     */
+    set(object, field: string, _default: any = null): QuestionBase<T> {
+        let value = _default;
+        if (hasIn(field, object)) {
+            value = object[field];
+        }
+        this[field] = value;
+        return this;
+    }
+
+    /**
+     * Sets the val property and returns this
+     * @param val The new val value
+     */
+    setVal(val: any = null) {
+        this.val = val;
+        return this;
+    }
 }
